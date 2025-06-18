4480ad68e5bccfbfe1b45b72364c0ea6fb45cd2c
Nguyen Tan Loc <126189862+tanloc04@users.noreply.github.com>
2025-06-18 17:55:13 +0700

    test new push

--- SnapBuyWeb/market.py
+++ SnapBuyWeb/market.py
@@ -5,4 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home_page():
+<<<<<<< HEAD
     return render_template('home.html')
+=======
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
+>>>>>>> loc
